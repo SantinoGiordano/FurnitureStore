dd5c184183e7d0f76bf4852e4ba58489128902fa
SantinoGiordano <santinogiordano13@gmail.com>
2025-03-04 11:00:24 -0500

    created a struct , not all the way done yet, and also simpl router

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type Furniture struct{
+	ID     string  `json:"id"`
+	Name   string `josn:"name"`
+	Description string `josn:"description"`
+	Price float64 `josn:"price"`
+}
+
+
 
 func main() {
-	fmt.Print("hello world")
+	router := gin.Default()
+	router.Get("/api/furniture", getFurniture)
+	// app.Post("/api/todos", createTodo)
+	// app.Patch("/api/todos/:id", updateTodo)
+	// app.Delete("/api/todos/:id", getFurniture)
+
+	router.Run(":8080")
 }
