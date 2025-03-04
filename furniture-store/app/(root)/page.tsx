15184876efee9897deb84a6b3e19eebd616c08c7
SantinoGiordano <santinogiordano13@gmail.com>
2025-03-04 13:28:00 -0500

    added a fetch request with use state

--- furniture-store/app/(root)/page.tsx
+++ furniture-store/app/(root)/page.tsx
@@ -1,7 +1,24 @@
-
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    async function fetchFurniture() {
+      const response = await fetch('http://localhost:8080/api/furniture');
+      const data = await response.json();
+      setItems(data);
+    }
+
+    fetchFurniture();
+  }, []); 
+
   return (
-    <div>Hello</div>
+    <ul>
+      {items.map((item) => (
+        <li key={item.id}>{item.name}</li> 
+      ))}
+    </ul>
   );
 }
