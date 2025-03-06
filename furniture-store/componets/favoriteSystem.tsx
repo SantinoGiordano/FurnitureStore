da248f1cd88c74c9572a004cb3f38d6a85508801
SantinoGiordano <santinogiordano13@gmail.com>
2025-03-06 18:59:48 -0500

    added a couple if statments for the logic in the heart change

--- furniture-store/componets/favoriteSystem.tsx
+++ furniture-store/componets/favoriteSystem.tsx
@@ -3,13 +3,28 @@ import React from "react";
 interface Furniture {
   favorite: boolean;
 }
+function changeHeart(item:Furniture){
+  if(item.favorite == false){
+    item.favorite = true
+    console.log("false >>> true")
+    // return <div>{ "❤️" }</div>
+  }
+  if(item.favorite == true){
+    item.favorite = false
+    console.log("true >>> false")
+  }
+}
 
 const Hearts = ({ favorite }: { favorite: boolean }) => {
   return <div>{favorite ? "❤️" : "🤍"}</div>;
 };
 
 const FavoriteSystem = ({ item }: { item: Furniture }) => {
-  return <Hearts favorite={item.favorite} />;
+  return(
+  <div onClick={()=>changeHeart(item)} className="hover:cursor-pointer">
+    <Hearts favorite={item.favorite} />
+  </div>
+  ) 
+  
 };
-
 export default FavoriteSystem;
