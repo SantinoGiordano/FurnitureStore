ae36288ea75ed57bb7965fe5def2307bc8666204
SantinoGiordano <santinogiordano13@gmail.com>
2025-03-20 10:41:07 -0400

    createed a file named store, this will hold all data for poeple who browse items , and will not have to create an account to have favorites or put in cart

---
+++ furniture-store/app/Store.tsx
@@ -0,0 +1,18 @@
+import { create } from "Zustand";
+
+type CounterStore = {
+  count: number;
+  increment: ()=> void
+  decrement: ()=> void
+};
+
+export const useCounterStore = create<CounterStore>((set) => ({
+  count: 0,
+  increment: () => {
+    set({count: 1})
+  },
+  decrement: () => {
+    set({count: -1})
+  },
+
+}));
